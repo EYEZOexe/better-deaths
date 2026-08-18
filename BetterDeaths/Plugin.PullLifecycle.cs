04c83879831a80121ade5ebf4071a0bb9b981738
EYEZO.exe <119551914+EYEZOexe@users.noreply.github.com>
2026-08-18 16:10:55 +0300

    chore: preserve existing pull lifecycle imports

--- BetterDeaths/Plugin.PullLifecycle.cs
+++ BetterDeaths/Plugin.PullLifecycle.cs
@@ -20,6 +20,7 @@ using FFXIVClientStructs.FFXIV.Client.Game.Object;
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using FFXIVClientStructs.FFXIV.Client.System.String;
 using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Client.UI.Shell;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using Lumina.Excel.Sheets;
 using System;
